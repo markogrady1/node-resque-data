28151388825eaed233d6d671f7e56693a0b0cc71
Mark O Grady <mark.o'grady@broadbean.com>
2021-07-09 16:49:09 +0100

    peeking in redis

--- src/Queue.js
+++ src/Queue.js
@@ -1,17 +1,48 @@
-import Job from './Job.js';
+// import Job from './Job.js';
+const Redis = require('ioredis');
 
-export default class Queue {
-  constructor () {
-      this.job = new Job();
 
+/*
+
+new Redis({
+  port: 6379, // Redis port
+  host: "127.0.0.1", // Redis host
+  family: 4, // 4 (IPv4) or 6 (IPv6)
+  password: "auth",
+  db: 0,
+});
+
+
+
+*/
+module.exports = class Queue {
+  constructor (config = undefined) {
+    this.namespace = config.namespace || 'resque'
+    this.redis = new Redis(config);
   }
 
-  async add(name, opts) {
+  async queue(queues, fn) {
+    let queueLen = [];
+    for (const index in queues) {
+      const length = await this.redis.llen(`${this.namespace}:queue:${queues[index]}`);
+      queueLen.push({
+        queue: queues[index],
+        num: length
+      });
+    }
 
-      opts = JSON.stringify(opts);
+          console.log(this.namespace);
 
-      const job = await this.job.create(name, opts, {});
 
-      console.log(job)
+    fn(queueLen);
+    // const keyPart = 'textkernelsearch-resque:queue';
+    // const hubIndexQueue = await this.redis.llen(`${keyPart}:hubIndex`);
+    // const candidateIndexQueue = await this.redis.llen(`${keyPart}:candidateIndex`);
+    // const delayedDeletionQueue = await this.redis.llen(`${keyPart}:delayedCandidateDelete`);
+
+    // console.log(this.namespace)
+    // console.log(hubIndexQueue)
+    // console.log(candidateIndexQueue)
+    // console.log(delayedDeletionQueue)
   }
 }
--- src/index.js
+++ src/index.js
@@ -1,10 +1,96 @@
-import Queue from './Queue.js';
+const  Queue = require('./queue.js');
+const  fs = require('fs');
+const  express = require('express');
+
+let queueInit;
+let generateHTML =  function (config, opts) {
+
+  if (config && typeof config === 'object') {
+    namespace: config.namespace || 'resque'
+  }
+
+  const html = fs.readFileSync(__dirname + '/queue-ui.html.tpl');
+    try {
+      fs.unlinkSync(__dirname + '/index.html');
+    } catch (e) {
+
+    }
+  var htmlWithCustomCss = html.toString().replace('<% customCss %>', '');
+
+  var js = fs.readFileSync(__dirname + '/queue-ui-init.js.tpl');
+  queueInit = js.toString().replace('<% swaggerOptions %>', '')
+  return htmlWithCustomCss;
+}
 
 
+let setup = function (config, opts) {
+  const html = generateHTML(config, opts)
+    const queue = new Queue(config);
 
+  return function (req, res) {
+     queue.queue(['math', 'Chicago', 'bar','hubIndexQueue'], (data) => {
+       console.log(data)
+       const newHtml = html.toString().replace('<% numbers %>', data[0].num);
+       res.send(newHtml)
+     })
+  };
+}
+
+var assetMiddleware = options => {
+  var opts = options || {}
+  opts.index = false
+
+  return express.static(getAbsoluteFSPath(), opts)
+}
+
+const getAbsoluteFSPath = function () {
+  // detect whether we are running in a browser or nodejs
+  if (typeof module !== "undefined" && module.exports) {
+    return require("path").resolve(__dirname)
+  }
+  throw new Error('getAbsoluteFSPath can only be called within a Nodejs environment');
+}
+
+const serve = [initFn, assetMiddleware()];
+
+function initFn (req, res, next) {
+  if (req.url === '/queue-ui-init.js') {
+    res.set('Content-Type', 'application/javascript')
+    console.log('serving', queueInit)
+    res.send(queueInit)
+  } else {
+    next()
+  }
+}
+
+
+var stringify = function (obj, prop) {
+  var placeholder = '____FUNCTIONPLACEHOLDER____';
+  var fns = [];
+  var json = JSON.stringify(obj, function (key, value) {
+    if (typeof value === 'function') {
+      fns.push(value);
+      return placeholder;
+    }
+    return value;
+  }, 2);
+  json = json.replace(new RegExp('"' + placeholder + '"', 'g'), function (_) {
+    return fns.shift();
+  });
+  return 'var options = ' + json + ';';
+};
+
+// setup()
+
+module.exports = {
+  setup: setup,
+  serve: serve,
+}
 
 // testing queue setup
 
-const queue = new Queue();
-queue.add('Chicago', {conn: "foo"});
+
+
+
+
 
